586227dc308ef21a551143485ed4f945f5799313
Nilton Gonzano <niltongr@outlook.com>
2022-03-01 11:12:42 -0500

    Initial commit

---
+++ app/src/main/java/com/ngonzano/delivery/activities/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.ngonzano.delivery.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.ngonzano.delivery.R
+
+class MainActivity : AppCompatActivity() {
+
+    var imageViewGoToRegister: ImageView? = null
+    var editTextEmail: EditText? = null
+    var editTextPassword: EditText? = null
+    var buttonLogin: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPassword = findViewById(R.id.edittext_password)
+        buttonLogin = findViewById(R.id.btn_login)
+
+        buttonLogin?.setOnClickListener{login()}
+        imageViewGoToRegister?.setOnClickListener{gotoRegister()}
+    }
+    private fun login(){
+        val email = editTextEmail?.text.toString()
+        val password = editTextPassword?.text.toString()
+
+        Toast.makeText(this, "El email es: $email", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "El password es: $password", Toast.LENGTH_SHORT).show()
+
+        Log.d("MainActivity", "El email es: $email")
+        Log.d("MainActivity", "El password es: $password")
+    }
+
+    fun String.isEmailValid():Boolean{
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+    private fun isValidForm(email: String, password: String): Boolean {
+        if (email.isBlank()){
+            return false
+        }
+        if (password.isBlank()){
+            return false
+        }
+        if (!email.isEmailValid()){
+            return false
+        }
+        return true
+    }
+    private fun gotoRegister(){
+        val i = Intent(this, RegisterActivity::class.java)
+        startActivity(i)
+    }
+}
---
+++ app/src/main/java/com/ngonzano/delivery/activities/RegisterActivity.kt
@@ -0,0 +1,64 @@
+package com.ngonzano.delivery.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.ngonzano.delivery.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    val TAG = "RegisterActivity"
+
+    var imageViewGoToLogin: ImageView? = null
+    var editTextName: EditText? = null
+    var editTextLastname: EditText? = null
+    var editTextEmail: EditText? = null
+    var editTextPhone: EditText? = null
+    var editTextPassword: EditText? = null
+    var editTextConfirmPassword: EditText? = null
+    var buttonRegister: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
+        editTextName = findViewById(R.id.edittext_name)
+        editTextLastname = findViewById(R.id.edittext_lastname)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPhone = findViewById(R.id.edittext_phone)
+        editTextPassword = findViewById(R.id.edittext_password)
+        editTextConfirmPassword = findViewById(R.id.edittext_confirm_password)
+        buttonRegister = findViewById(R.id.btn_register)
+
+        imageViewGoToLogin?.setOnClickListener{
+            goToLogin()
+        }
+        buttonRegister?.setOnClickListener{
+            register()
+        }
+    }
+    private fun register(){
+        val name = editTextName?.text.toString()
+        val lastname = editTextLastname?.text.toString()
+        val email = editTextEmail?.text.toString()
+        val phone = editTextPhone?.text.toString()
+        val password = editTextPassword?.text.toString()
+        val confirmPassword = editTextConfirmPassword?.text.toString()
+
+         Log.d(TAG, "El nombre es: $name")
+         Log.d(TAG, "El Apellido es: $name")
+         Log.d(TAG, "El Email es: $name")
+         Log.d(TAG, "El Telefono es: $name")
+         Log.d(TAG, "El Contraseña es: $name")
+         Log.d(TAG, "El confirm password es: $name")
+    }
+    private fun goToLogin(){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+}
